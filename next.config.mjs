1e02d629ba5cf79832ec1c794a528940d59fc0eb
fhok2 <138731655+fhok2@users.noreply.github.com>
2025-02-20 20:57:52 -0300

    atualiza next.config

--- next.config.mjs
+++ next.config.mjs
@@ -5,13 +5,11 @@ const nextConfig = {
         config.externals = [
           ...(config.externals || []), 
           'encoding', 
-          '_http_common'
+          '@prisma/client/runtime'
         ]
       }
       return config
     },
-    // Desabilite a otimização do swc para resolver potenciais problemas com Prisma
-    swcMinify: false,
   };
   
   export default nextConfig;
